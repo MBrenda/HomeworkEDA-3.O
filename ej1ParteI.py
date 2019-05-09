0b59c08d33f5b5b6c5bb22801ac2ce1137377533
Brenda Martinez <b.martinezlujan@gmail.com>
2019-05-08 22:26:27 -0300

    listo

--- ej1ParteI.py
+++ ej1ParteI.py
@@ -4,15 +4,31 @@ import random
 tamanio = 4
 num = ('0','1','2','3','4','5','6','7','8','9')
 num_ale = random.choice(num)
-print (num_ale)
+#print (num_ale)
 cifra='' 
 
 for i in range(tamanio):
     while num_ale in cifra:
         num_ale = random.choice(num)
     cifra = cifra + num_ale
-    print ('cifra=', cifra)
+    #print ('cifra=', cifra)
 
 print ('Hola ADIVINADOR tienes que acertar el numero de', tamanio, 'cifras que estoy pensando')
 num_adivinador = input('Adivinador:')
-print('Adivinador:', num_adivinador) 
+#print('Adivinador:', num_adivinador) 
+
+while num_adivinador != cifra:
+    orden_igual = 0
+    num_iguales = 0
+
+    for i in range(tamanio):
+        if num_adivinador[i] == cifra[i]:
+            orden_igual = orden_igual + 1
+        elif num_adivinador[i] in cifra:
+            num_iguales = num_iguales + 1
+    
+    print ('Pensador: respuesta', orden_igual, 'bien,', num_iguales, 'regular')
+    print('Estan en el lugar correcto:', orden_igual, 'Estan en el lugar incorrecto:' , num_iguales)
+    num_adivinador = input("Intenta de nuevo: ")
+
+print ("------------------¡JUEGO TERMINADO!------------------")
